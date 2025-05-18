f6ad4fd3390143ff5ddd69b1ca7d55ba5d3d3478
kyneccc <121186321+kyneccc@users.noreply.github.com>
2025-05-18 13:33:39 +0300

    Delete semifinal directory

--- semifinal/configure-cloudinfra.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ansible-playbook /home/altlinux/bin/playbook/hap.yml 2> /dev/null 
-ansible-playbook /home/altlinux/bin/playbook/web.yml  2> /dev/null
-ansible-playbook /home/altlinux/bin/playbook/db.yml  2> /dev/null
-
